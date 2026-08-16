8375b79b4dc255908276efc93ec5ac4482631975
Pedro <pedrossantos0715@gmail.com>
2026-08-16 02:14:44 -0300

    algumas correções / versão temporária

--- ex01.c
+++ ex01.c
@@ -6,51 +6,80 @@ int main() {
 	int op = 0;
 	int qtd_nomes = 0;
 
-	char *nomes;
+	char *nomes = NULL;
 
 	while (op != 4) {
-		printf("1. Adicionar Nome\n2. Remover Nome\n3. Listar\n4. Sair\nOperacao: ");
+		printf("\n1. Adicionar Nome\n2. Remover Nome\n3. Listar\n4. Sair\nOperacao: ");
 		scanf("%d", &op);
 
 		switch(op) {
-			case 1:
+			case 1: {
 				char nome[20];
 				printf("Nome: ");
 				scanf("%s", nome);
-				if (qtd_nomes == 0)
-					nomes = (char *) malloc((strlen(nome) + 1) * sizeof(char));
-				else {
-					realloc(nomes, (strlen(nomes) + strlen(nome) + 2) * sizeof(char));
-					strcat(nomes, ";"); // separador
+
+				if (qtd_nomes == 0) {
+					nomes = (char *) malloc((strlen(nome) + 2) * sizeof(char)); // +2 (separador e terminador)
+					strcpy(nomes, nome); // preciso do cpy pra limpar a "sujeira"
+				} else {
+					nomes = (char *) realloc(nomes, (strlen(nomes) + strlen(nome) + 2) * sizeof(char)); 
+					strcat(nomes, nome);
 				}
 
-				// adicionar nome
-				strcat(nomes, nome);
-
+				strcat(nomes, ";");
 				qtd_nomes++;
 				break;
-			case 2:
-				// remover nome (strtok?)
-				char nome[20];
+			}
+			case 2: {
+				if (nomes == NULL)
+					break;
+
+				int nomes_buscados = 0, encontrado = 0;
+				char nome[20], *busca, *aux;
 				printf("Nome: ");
 				scanf("%s", nome);
 
-				int tamanho = strlen(nome);
+				aux = (char *) malloc((strlen(nomes) + 1) * sizeof(char)); // solucao temporaria
+				strcpy(aux, nomes);
 
-				for (int i = 0; nomes[i] != '\0'; i++) {
-					nome = strtok(nomes, ";");
+				busca = strtok(aux, ";");
 
-					// fico salvando o strtok atual em nomes e dando realloc, se encontrar igual, nao da realloc naquela vez
-					// no final ou eu vou refazer a string inteira, ou eu vou refazer ela pulando o nome
+				while (busca != NULL) {
+					if (strcmp(nome, busca) != 0) {
+						if (nomes_buscados == 0) // se for o primeiro nome, preciso copiar, nao concatenar (diminui a string)
+							strcpy(nomes, busca);
+						else
+							strcat(nomes, busca);
+						strcat(nomes, ";");
+						nomes_buscados++;
+					} else {
+						encontrado = 1;
+					}
 
+					busca = strtok(NULL, ";");
 				}
-				// se encontrado: qtd_nomes--;
+
+				if (encontrado) {
+					qtd_nomes--;
+					if (nomes_buscados == 0) // nomes tinha um unico nome que foi excluido
+						nomes[0] = '\0';
+					nomes = (char *) realloc(nomes, (strlen(nomes) + 1) * sizeof(char));
+				}
+				free(aux);
 				break;
-			case 3:
-				printf("%s\n", nomes);
+			}
+			case 3: {
+				for (int i = 0; nomes != NULL && nomes[i] != '\0'; i++){
+					if (nomes[i] != ';')
+						printf("%c", nomes[i]);
+					else
+						printf(" ");
+				}
 				break;
-			default:
+			} 
+			default: {
 				break;	
+			}
 		}
 	}
 
